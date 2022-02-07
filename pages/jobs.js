151f58775b8c2d7854b6d079d53575a6e7bef0dd
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2022-02-06 23:44:34 -0500

    Commit Inicial

---
+++ pages/jobs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const jobs = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        My Jobs!
+      </h1>
+    </div>
+  );
+};
+
+export default jobs;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        /* Colors */
+        'blue-500': '#3b82f6',
+        'blue-700': '#1d4ed8',
+        'blue-900': '#1e3a8a',
+      }
+    },
+  },
+  plugins: [],
+}
